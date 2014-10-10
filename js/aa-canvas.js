8b031143231b4b8820df2dc1d0fe659c314503d2
setchi_ <iphase56@ezweb.ne.jp>
2014-10-10 10:04:53 +0900

    プライベートメンバ隠蔽

--- js/aa-canvas.js
+++ js/aa-canvas.js
@@ -3,60 +3,59 @@
  */
 
 var AACanvas = function (canvas) {
-	this.cfw = 1;
-	this.cfh = 1;
-	this.WPH = 1.7;
-	this._scale = 1.25;
-	this._canvas = canvas;
-	this.draw = this.draw();
-}
-AACanvas.prototype = {
-	adjustScale: function (video) {
-		this.WPH = video.videoWidth / video.videoHeight;
+	var module = {},
+		scale = 1.25,
+		innerText = canvas.innerText ? "innerText" : "textContent";
 
-		var width  = 1600 * this._scale;
-		var height = (width / this.WPH | 0) || 1;
-		this.cfw = (width / 10) | 0;
-		this.cfh = (this.cfw / this.WPH * 0.6) | 0;
+	function calcScale (per) {
+		return 0.5 + (per / 10);
+	}
 
-		$(this._canvas).css({
+	module.cfw = 1;
+	module.cfh = 1;
+	module.WPH = 1.7;
+
+	module.adjustScale = function (video) {
+		module.WPH = video.videoWidth / video.videoHeight;
+
+		var width  = 1600 * scale;
+		var height = (width / module.WPH | 0) || 1;
+		module.cfw = (width / 10) | 0;
+		module.cfh = (module.cfw / module.WPH * 0.6) | 0;
+
+		$(canvas).css({
 			width: width + 'px',
 			height: height + 'px',
 			left: -(width >> 1) + 'px'
 		});
-		this.resize();
+		module.resize();
 	},
 
-	applyScale: function (video, rate) {
-		this._scale = this._calcScale(rate);
-		this.adjustScale(video);
+	module.applyScale = function (video, rate) {
+		scale = calcScale(rate);
+		module.adjustScale(video);
 	},
 
-	draw: function () {
-		var innerText = this._canvas.innerText ? "innerText" : "textContent";
-
-		return function (text) {
-			this._canvas[innerText] = text;
-		}
+	module.draw = function (text) {
+		console.log('draw')
+		canvas[innerText] = text;
 	},
 
-	resize: function () {
-		var width = parseInt($(this._canvas).width());
-		var height = parseInt($(this._canvas).height());
+	module.resize = function () {
+		var width = parseInt($(canvas).width());
+		var height = parseInt($(canvas).height());
 
 		var scale = Math.min(
 			$(window).width() * 0.97 / width, 
 			($(window).height() * 0.97) / height
 		);
 
-		$(this._canvas).css({
+		$(canvas).css({
 			'-webkit-transform': 'scale(' + scale + ', ' + scale + ')',
 			'transform': 'scale(' + scale + ', ' + scale + ')',
 			'top': (height - height * scale) / -2 + 'px'
 		});
-	},
-
-	_calcScale: function (per) {
-		return 0.5 + (per / 10);
 	}
+
+	return module;
 }
